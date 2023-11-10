f9a02e220ac7a83b70e80712871a2f15266dd8e0
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-11-10 12:54:27 -0300

    Added the rest of tables

--- snapcorrect/db/schema.sql
+++ snapcorrect/db/schema.sql
@@ -1,8 +1,32 @@
 DROP TABLE IF EXISTS professor;
+DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS student;
+DROP TABLE IF EXISTS student_correction;
 
-CREATE TABLE
-    professor (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
-    );
+CREATE TABLE professor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    professor_id INTEGER REFERENCES professor(id),
+    title TEXT NOT NULL,
+    date DATE,
+    template BLOB
+);
+
+CREATE TABLE student (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    professor_id INTEGER REFERENCES professor(id)
+);
+
+CREATE TABLE student_correction (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    test_id INTEGER REFERENCES test(id),
+    student_id INTEGER REFERENCES student(id),
+    score INTEGER,
+    corrected_test BLOB
+);
