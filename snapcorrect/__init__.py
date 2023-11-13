8b545bd78d751a81ebde9690fd0b5bfc1c00b0ca
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-11-13 10:23:51 -0300

    Added file extension support

--- snapcorrect/__init__.py
+++ snapcorrect/__init__.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
 
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "snapcorrect.sqlite")
+        DATABASE=os.path.join(app.instance_path, "snapcorrect.sqlite"),
+        ALLOWED_EXTENSIONS={"png", "jpg"}
     )
 
     if test_config is None:
